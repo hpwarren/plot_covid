b1144262c709d627718580a56e87b1db23425efd
Harry Warren <harry.warren@nrl.navy.mil>
2020-04-20 06:16:18 -0400

    mods

--- plot_covid_2.py
+++ plot_covid_2.py
@@ -8,6 +8,7 @@ from scipy.optimize import curve_fit
 from convert_time import convert_time
 from datetime import datetime, timedelta
 import sys
+import hpw
         
 def funct(x, a, b):
     # f = a*np.exp(b*x)
@@ -24,6 +25,7 @@ class update_covid:
         self.read_data()
         self.fit_data(remove=remove)
         self.calc_ratio()
+        self.calc_daily()
         self.plot_data()
 
     def get_data(self):
@@ -35,7 +37,6 @@ class update_covid:
     def read_data(self):
         df = pd.read_csv(self.filename)
         md = df[df['state'] == self.state]
-        print(md)
         dates = np.flip(np.array(md.date)) # read dates in YYYYMMDD format
         dates = convert_time(dates) # convert to datetime
         nday = np.arange(len(md)) # dates as integer array
@@ -43,6 +44,21 @@ class update_covid:
         self.data['dates'] = dates
         self.data['positive'] = np.flip(np.array(md.positive))
         self.data['deaths'] = np.flip(np.array(md.death))
+
+    def calc_daily(self):
+        nday = self.data['nday']
+        deaths = self.data['deaths']
+        pos = self.data['positive']
+        dates = self.data['dates']
+        nmax = len(nday)
+
+        self.daily = []
+        for n in range(1, nmax):
+            d_pos = pos[n] - pos[n-1]
+            d_deaths = deaths[n] - deaths[n-1]
+            if not np.isfinite(d_deaths): continue
+            print( (nday[n], dates[n], d_pos, d_deaths) )
+            self.daily.append( (nday[n], dates[n], d_pos, d_deaths) )
 
     def fit_data(self, remove=0):
         x = self.data['nday']
@@ -89,6 +105,7 @@ class update_covid:
             else:
                 act_deaths = '---'
                 act_pos = '---'
+
             print(f'{n:3d}{d:>15}{act_pos:>10}{est_pos:10,.0f}'\
                   f'{est_deaths:10,.0f}{act_deaths:>10}')
         
@@ -98,20 +115,38 @@ class update_covid:
         t, pfit = self.extrapolate_fit()
         ratio = self.calc_ratio()
 
+        xlim = (np.amin(t), np.amax(t))
         period = 1/self.popt[1]
         title = f'COVID Data for {self.state} | Doubling Period = {period:0.1f} days'\
             f' | Death Ratio = {ratio:0.3f}'
 
-        plt.semilogy(self.data['nday'], self.data['positive'], 'o', color='C0', \
+        g = hpw.graphics.image_grid(image_dim=[600,600/2], n_rows=2, n_cols=1, \
+                                    figure_margin=[60,40,0,25])
+        fig = g.get_figure()
+
+        pos = g.get_plot_pos(0)
+        ax = plt.axes(pos)
+        ax.semilogy(self.data['nday'], self.data['positive'], 'o', color='C0', \
                      label='Actual Positive')
-        plt.semilogy(t, pfit*ratio, ':', label='Modeled Positive', color='C0')
-        plt.semilogy(self.data['nday'], self.data['deaths'], 'or', label='Actual Deaths')
-        plt.semilogy(t, pfit, ':r', label='Modeled Deaths')
-        plt.xlabel('Days Since March 4, 2020')
-        plt.ylabel('Cummulative Positive Cases')
-        plt.title(title, fontsize=10)
-        plt.ylim((1,1.0E+6))
-        plt.legend()
+        ax.semilogy(t, pfit*ratio, ':', label='Modeled Positive', color='C0')
+        ax.semilogy(self.data['nday'], self.data['deaths'], 'or', label='Actual Deaths')
+        ax.semilogy(t, pfit, ':r', label='Modeled Deaths')
+        ax.set_ylabel('Cummulative')
+        ax.set_title(title, fontsize=10)
+        ax.set_ylim((1,1.0E+6))
+        ax.set_xlim(xlim)
+        ax.set_xticklabels(' ')
+        ax.legend()
+
+        pos = g.get_plot_pos(1)
+        ax = plt.axes(pos)
+        t, dates, d_pos, d_deaths = zip(*self.daily)
+        ax.bar(t, d_deaths, color='red', label='Actual Deaths')
+        ax.set_xlim(xlim)
+        ax.set_xlabel('Days Since March 4, 2020')
+        ax.set_ylabel('Daily')
+        ax.legend(loc='upper left')        
+        
         plt.savefig('plot_covid.png')
         plt.show()
 
